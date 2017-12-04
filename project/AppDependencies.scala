ef5252c48c46e254ea5ed0e0c6be0e0fa9f06bcd
Stew Abel <stew@grupettosoftware.com>
2017-12-04 16:57:25 +0000

    CDD-1192 adding initial dependencies, config and switching to microse… (#1)
    * CDD-1192 adding initial dependencies, config and switching to microservice-bootstrap
    
    * CDD-1192 removing unrequired code
    
    * removing unrequired config section comments
    
    * reordering dependencies
    
    * removing deprecated logger config

---
+++ project/AppDependencies.scala
@@ -0,0 +1,32 @@
+import sbt._
+
+object AppDependencies {
+  private val microserviceBootstrapVersion = "6.12.0"
+  private val authClientVersion = "2.3.0"
+  private val xmlResolverVersion = "1.2"
+  private val hmrcTestVersion = "3.0.0"
+  private val scalaTestVersion = "3.0.4"
+  private val scalatestplusVersion = "2.0.1"
+  private val mockitoVersion = "2.6.2"
+  private val pegdownVersion = "1.6.0"
+  private val wireMockVersion = "2.10.1"
+  private val testScope = "test,it"
+
+  val microserviceBootStrap = "uk.gov.hmrc" %% "microservice-bootstrap" % microserviceBootstrapVersion
+
+  val authClient =  "uk.gov.hmrc" %% "auth-client" % authClientVersion
+
+  val xmlResolver = "xml-resolver" % "xml-resolver" % xmlResolverVersion
+
+  val hmrcTest = "uk.gov.hmrc" %% "hmrctest" % hmrcTestVersion % testScope
+
+  val scalaTest = "org.scalatest" %% "scalatest" % scalaTestVersion % testScope
+
+  val pegDown = "org.pegdown" % "pegdown" % pegdownVersion % testScope
+
+  val scalaTestPlusPlay = "org.scalatestplus.play" %% "scalatestplus-play" % scalatestplusVersion % testScope
+
+  val wireMock = "com.github.tomakehurst" % "wiremock" % wireMockVersion % testScope exclude("org.apache.httpcomponents","httpclient") exclude("org.apache.httpcomponents","httpcore")
+
+  val mockito =  "org.mockito" % "mockito-core" % mockitoVersion % testScope
+}
