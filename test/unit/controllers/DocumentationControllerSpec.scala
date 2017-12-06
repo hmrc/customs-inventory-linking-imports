b18c0b1e028d1745f9b0120f99a4c5949679c5da
Stew Abel <stew@grupettosoftware.com>
2017-12-06 11:05:52 +0000

    CDD-1192 api definition
    * CDD-1192 adding RAML api definition
    
    * CDD-1192 adding api definition
    
    * CDD-1192 updating readme with basic information about the service
    
    * CDD-1192 removing commented out config
    
    * CDD-1192 adding RAML api definition
    
    * CDD-1192 adding api definition
    
    * CDD-1192 removing commented out config

---
+++ test/unit/controllers/DocumentationControllerSpec.scala
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package unit.controllers
+
+import play.api.Configuration
+import play.api.http.HeaderNames.CONTENT_TYPE
+import play.api.http.Status.OK
+import play.api.libs.json.Json
+import play.api.test.FakeRequest
+import uk.gov.hmrc.customs.inventorylinking.imports.controllers.DocumentationController
+import uk.gov.hmrc.customs.inventorylinking.imports.views.txt
+import uk.gov.hmrc.play.test.{UnitSpec, WithFakeApplication}
+
+class DocumentationControllerSpec extends UnitSpec with WithFakeApplication{
+
+  implicit val materializer = fakeApplication.materializer
+
+  "with empty configuration DocumentationController.defintion" should {
+    "throw IllegalStateException" in {
+      val controller = new DocumentationController(Configuration())
+      val thrown = the[IllegalStateException] thrownBy getDefinition(controller)
+      thrown.getMessage should equal("customs.definition.api-scope is not configured")
+    }
+  }
+
+  "With valid configuration DocumentationController.definition" should {
+    val apiScope = "scope"
+    val config = Configuration("customs.definition.api-scope" -> apiScope)
+    val controller = new DocumentationController(config)
+    val result = getDefinition(controller)
+
+    "return OK status" in {
+      status(result) shouldBe OK
+    }
+
+    "have a JSON content type" in {
+      result.header.headers should contain (CONTENT_TYPE -> "application/json;charset=utf-8")
+    }
+
+    "return definition in the body" in {
+      jsonBodyOf(result) shouldBe Json.parse(txt.definition("scope").toString())
+    }
+  }
+
+  private def getDefinition(controller: DocumentationController) = {
+    await(controller.definition().apply(FakeRequest()))
+  }
+}
