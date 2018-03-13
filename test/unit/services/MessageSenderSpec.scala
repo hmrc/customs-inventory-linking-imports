a48e6cf3c9995cde41039edb8e8e53af8fb5d5d7
Marcin Cichocki <marcin.cichocki@digital.hmrc.gov.uk>
2018-03-13 11:36:14 +0000

    MessageSenderSpec

--- test/unit/services/MessageSenderSpec.scala
+++ test/unit/services/MessageSenderSpec.scala
@@ -18,12 +18,13 @@ package unit.services
 
 import java.net.URLEncoder
 
-import org.mockito.ArgumentMatchers.{eq => ameq}
+import org.mockito.ArgumentMatchers.{eq => ameq, any}
 import org.mockito.Mockito.when
 import org.scalatest.mockito.MockitoSugar
 import org.scalatest.prop.TableDrivenPropertyChecks
 import org.scalatest.{Matchers, WordSpecLike}
 import play.api.http.Status.ACCEPTED
+import play.api.mvc.{AnyContent, Request}
 import uk.gov.hmrc.customs.inventorylinking.imports.connectors.{ApiSubscriptionFieldsConnector, ImportsConnector, OutgoingRequest, OutgoingRequestBuilder}
 import uk.gov.hmrc.customs.inventorylinking.imports.model._
 import uk.gov.hmrc.customs.inventorylinking.imports.services._
@@ -36,84 +37,87 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
 class MessageSenderSpec extends UnitSpec with Matchers with MockitoSugar with TableDrivenPropertyChecks {
-//
-//  trait SetUp {
-//    protected val importsMessageType: ImportsMessageType
-//
-//    val httpResponse: AnyRef with HttpResponse = HttpResponse(ACCEPTED)
-//    val outgoingRequestBuilder = mock[OutgoingRequestBuilder]
-//    lazy val goodsArrivalXmlValidationService: GoodsArrivalXmlValidationService = mock[GoodsArrivalXmlValidationService]
-//    lazy val validateMovementXmlValidationService: ValidateMovementXmlValidationService = mock[ValidateMovementXmlValidationService]
-//    val importsConnector: ImportsConnector = mock[ImportsConnector]
-//    val apiSubscriptionFieldsConnector = mock[ApiSubscriptionFieldsConnector]
-//    val headers: Map[String, String] = Map(HeaderNames.XClientId -> TestXClientId, HeaderNames.XBadgeIdentifier -> XBadgeIdentifierHeaderValueAsString)
-//    val sender: MessageSender = new MessageSender(apiSubscriptionFieldsConnector, outgoingRequestBuilder, goodsArrivalXmlValidationService, validateMovementXmlValidationService, importsConnector)
-//    lazy val outgoingRequest = OutgoingRequest(serviceConfig, body, requestInfo)
-//    private val apiContextEncoded = URLEncoder.encode("customs/inventory-linking-imports", "UTF-8")
-//
-//    implicit val mockHeaderCarrier: HeaderCarrier = mock[HeaderCarrier]
-//
-//    protected def service: XmlValidationService = importsMessageType match {
-//      case GoodsArrival => goodsArrivalXmlValidationService
-//      case ValidateMovement => validateMovementXmlValidationService
-//    }
-//
-//    when(apiSubscriptionFieldsConnector.getSubscriptionFields(ameq(ApiSubscriptionKey(TestXClientId, apiContextEncoded, "1.0")))(ameq(mockHeaderCarrier))).thenReturn(Future.successful(TestApiSubscriptionFieldsResponse))
-//  }
-//
-//  val messageTypes = Table(
-//    "Message type",
-//    GoodsArrival,
-//    ValidateMovement
-//  )
-//
-//  forAll(messageTypes){(messageType) =>
-//    s"$messageType send" when {
-//      "message is valid" should {
-//        "return the result from the connector" in new SetUp {
-//          val importsMessageType: ImportsMessageType = messageType
-//          when(outgoingRequestBuilder.build(messageType, requestInfo, TestFieldsId, XBadgeIdentifierHeaderValue, body)).thenReturn(outgoingRequest)
-//          when(service.validate(body)).thenReturn(Future.successful(()))
-//          when(importsConnector.post(outgoingRequest)).thenReturn(Future.successful(httpResponse))
-//
-//          val actualResponse = await(sender.validateAndSend(messageType, body, requestInfo, headers))
-//
-//          actualResponse shouldBe httpResponse
-//        }
-//      }
-//
-//      "message is invalid" should {
-//        "return failed future when validation service throws an exception" in new SetUp {
-//          val importsMessageType: ImportsMessageType = messageType
-//          when(service.validate(body)).thenReturn(Future.failed(emulatedServiceFailure))
-//
-//          val caught = intercept[EmulatedServiceFailure](await(sender.validateAndSend(messageType, body, requestInfo, headers)))
-//
-//          caught shouldBe emulatedServiceFailure
-//        }
-//
-//        "return failed future when expected header X-Client-ID is not present" in new SetUp {
-//          val importsMessageType: ImportsMessageType = messageType
-//          when(service.validate(body)).thenReturn(Future.successful(()))
-//          val headersWithOnlyXBadgeIdentifier = Map(HeaderNames.XBadgeIdentifier -> XBadgeIdentifierHeaderValueAsString)
-//
-//          val caught = intercept[IllegalStateException](await(sender.validateAndSend(messageType, body, requestInfo, headersWithOnlyXBadgeIdentifier)))
-//
-//          caught.getMessage shouldBe "Invalid request"
-//        }
-//
-//        "return failed future when expected header X-Badge-ID is not present" in new SetUp {
-//          val importsMessageType: ImportsMessageType = messageType
-//          when(service.validate(body)).thenReturn(Future.successful(()))
-//          val headersWithOnlyXClientId = Map(HeaderNames.XClientId -> TestXClientId)
-//
-//          val caught = intercept[IllegalStateException](await(sender.validateAndSend(messageType, body, requestInfo, headersWithOnlyXClientId)))
-//
-//          caught.getMessage shouldBe "Invalid request"
-//        }
-//      }
-//
-//    }
-//  }
-//TODO MC fix
+
+  trait SetUp {
+    protected val importsMessageType: ImportsMessageType
+
+    val httpResponse: AnyRef with HttpResponse = HttpResponse(ACCEPTED)
+    val outgoingRequestBuilder = mock[OutgoingRequestBuilder]
+    lazy val goodsArrivalXmlValidationService: GoodsArrivalXmlValidationService = mock[GoodsArrivalXmlValidationService]
+    lazy val validateMovementXmlValidationService: ValidateMovementXmlValidationService = mock[ValidateMovementXmlValidationService]
+    val importsConnector: ImportsConnector = mock[ImportsConnector]
+    val apiSubscriptionFieldsConnector = mock[ApiSubscriptionFieldsConnector]
+    val headers: Map[String, String] = Map(HeaderNames.XClientId -> TestXClientId, HeaderNames.XBadgeIdentifier -> XBadgeIdentifierHeaderValueAsString)
+    val sender: MessageSender = new MessageSender(apiSubscriptionFieldsConnector, outgoingRequestBuilder, goodsArrivalXmlValidationService, validateMovementXmlValidationService, importsConnector)
+    lazy val outgoingRequest = OutgoingRequest(serviceConfig, body, requestInfo)
+    private val apiContextEncoded = URLEncoder.encode("customs/inventory-linking-imports", "UTF-8")
+
+    implicit val mockHeaderCarrier: HeaderCarrier = mock[HeaderCarrier]
+    val request: Request[AnyContent] = mock[Request[AnyContent]]
+    implicit val data: RequestDataWrapper = mock[RequestDataWrapper]
+
+    protected def service: XmlValidationService = importsMessageType match {
+      case GoodsArrival => goodsArrivalXmlValidationService
+      case ValidateMovement => validateMovementXmlValidationService
+    }
+
+    when(apiSubscriptionFieldsConnector.getSubscriptionFields(ameq(ApiSubscriptionKey(TestXClientId, apiContextEncoded, "1.0")))(any[RequestDataWrapper])).thenReturn(Future.successful(TestApiSubscriptionFieldsResponse))
+    when(data.body).thenReturn(body)
+    when(data.requestInfo).thenReturn(requestInfo)
+  }
+
+  val messageTypes = Table(
+    "Message type",
+    GoodsArrival,
+    ValidateMovement
+  )
+
+  forAll(messageTypes){(messageType) =>
+    s"$messageType send" when {
+      "message is valid" should {
+        "return the result from the connector" in new SetUp {
+          val importsMessageType: ImportsMessageType = messageType
+          when(outgoingRequestBuilder.build(messageType, requestInfo, TestFieldsId, XBadgeIdentifierHeaderValue, body)).thenReturn(outgoingRequest)
+          when(service.validate(body)).thenReturn(Future.successful(()))
+          when(importsConnector.post(outgoingRequest)).thenReturn(Future.successful(httpResponse))
+          when(data.headers).thenReturn(headers)
+          val actualResponse = await(sender.validateAndSend(messageType))
+
+          actualResponse shouldBe httpResponse
+        }
+      }
+
+      "message is invalid" should {
+        "return failed future when validation service throws an exception" in new SetUp {
+          val importsMessageType: ImportsMessageType = messageType
+          when(service.validate(body)).thenReturn(Future.failed(emulatedServiceFailure))
+          when(data.headers).thenReturn(headers)
+          val caught = intercept[EmulatedServiceFailure](await(sender.validateAndSend(messageType)))
+
+          caught shouldBe emulatedServiceFailure
+        }
+
+        "return failed future when expected header X-Client-ID is not present" in new SetUp {
+          val importsMessageType: ImportsMessageType = messageType
+          when(service.validate(body)).thenReturn(Future.successful(()))
+          val headersWithOnlyXBadgeIdentifier = Map(HeaderNames.XBadgeIdentifier -> XBadgeIdentifierHeaderValueAsString)
+          when(data.headers).thenReturn(headersWithOnlyXBadgeIdentifier)
+          val caught = intercept[IllegalStateException](await(sender.validateAndSend(messageType)))
+
+          caught.getMessage shouldBe "Invalid request"
+        }
+
+        "return failed future when expected header X-Badge-ID is not present" in new SetUp {
+          val importsMessageType: ImportsMessageType = messageType
+          when(service.validate(body)).thenReturn(Future.successful(()))
+          val headersWithOnlyXClientId = Map(HeaderNames.XClientId -> TestXClientId)
+          when(data.headers).thenReturn(headersWithOnlyXClientId)
+          val caught = intercept[IllegalStateException](await(sender.validateAndSend(messageType)))
+
+          caught.getMessage shouldBe "Invalid request"
+        }
+      }
+
+    }
+  }
 }
