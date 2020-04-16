9895d659a55a6f0887654d7d431a2859adf80f6c
Tim <glidester@gmail.com>
2020-04-16 14:19:27 +0100

    Adding new test for circuit breaker

---
+++ test/integration/ImportsConnectorSpec.scala
@@ -0,0 +1,153 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package integration
+
+import java.util.UUID
+
+import akka.pattern.CircuitBreakerOpenException
+import org.joda.time.DateTime
+import org.scalatest.prop.TableDrivenPropertyChecks
+import org.scalatestplus.mockito.MockitoSugar
+import org.scalatestplus.play.guice.GuiceOneAppPerSuite
+import play.api.Application
+import play.api.inject.guice.GuiceApplicationBuilder
+import play.api.test.Helpers._
+import uk.gov.hmrc.customs.inventorylinking.imports.connectors.ImportsConnector
+import uk.gov.hmrc.customs.inventorylinking.imports.model.GoodsArrival
+import uk.gov.hmrc.customs.inventorylinking.imports.model.actionbuilders.ValidatedPayloadRequest
+import uk.gov.hmrc.http._
+import uk.gov.hmrc.http.logging.Authorization
+import util.ExternalServicesConfig.{AuthToken, Host, Port}
+import util.TestData
+import util.XMLTestData.ValidInventoryLinkingMovementRequestXML
+import util.externalservices.InventoryLinkingImportsExternalServicesConfig._
+import util.externalservices.{ApiSubscriptionFieldsService, InventoryLinkingImportsService}
+
+import scala.xml.NodeSeq
+
+class ImportsConnectorSpec extends IntegrationTestSpec with InventoryLinkingImportsService with GuiceOneAppPerSuite with MockitoSugar
+  with ApiSubscriptionFieldsService with TableDrivenPropertyChecks {
+
+  private val numberOfCallsToTriggerStateChange = 5
+  private val unstablePeriodDurationInMillis = 200
+  private val unavailablePeriodDurationInMillis = 250
+
+  override implicit lazy val app: Application =
+    new GuiceApplicationBuilder().configure(Map(
+      "circuitBreaker.numberOfCallsToTriggerStateChange" -> numberOfCallsToTriggerStateChange,
+      "circuitBreaker.unstablePeriodDurationInMillis" -> unstablePeriodDurationInMillis,
+      "circuitBreaker.unavailablePeriodDurationInMillis" -> unavailablePeriodDurationInMillis,
+      "microservice.services.goodsarrival.host" -> Host,
+      "microservice.services.goodsarrival.port" -> Port,
+      "microservice.services.goodsarrival.context" -> GoodsArrivalConnectorContext,
+      "microservice.services.goodsarrival.bearer-token" -> AuthToken
+    )).build()
+
+  private lazy val connector = app.injector.instanceOf[ImportsConnector]
+
+  private val incomingBearerToken = "some_client's_bearer_token"
+  private val incomingAuthToken = s"Bearer $incomingBearerToken"
+  private val correlationId = UUID.randomUUID()
+  private implicit val vpr = TestData.TestCspValidatedPayloadRequest
+
+  private implicit val hc: HeaderCarrier = HeaderCarrier(authorization = Some(Authorization(incomingAuthToken)))
+
+  override protected def beforeAll() {
+    startMockServer()
+  }
+
+  override protected def afterEach(): Unit = {
+    resetMockServer()
+  }
+
+  override protected def afterAll() {
+    stopMockServer()
+  }
+
+    "ImportsConnector" should {
+
+      //wait to clear the circuit breaker state that may of been tripped by previous tests
+      Thread.sleep(unavailablePeriodDurationInMillis)
+
+      "make a correct request" in {
+        startImportsService(GoodsArrivalConnectorContext)
+
+        await(sendValidXml(ValidInventoryLinkingMovementRequestXML))
+
+        verifyImportsConnectorServiceWasCalledWith(GoodsArrivalConnectorContext, ValidInventoryLinkingMovementRequestXML.toString())
+      }
+
+      "cause circuit breaker to trip after specified number of failures" in {
+        Thread.sleep(unavailablePeriodDurationInMillis)
+
+        startBackendService(INTERNAL_SERVER_ERROR)
+
+        1 to numberOfCallsToTriggerStateChange foreach { _ =>
+          val k = intercept[Upstream5xxResponse](await(sendValidXml(ValidInventoryLinkingMovementRequestXML)))
+          k.reportAs shouldBe BAD_GATEWAY
+        }
+
+        1 to 3 foreach { _ =>
+          intercept[CircuitBreakerOpenException](await(sendValidXml(ValidInventoryLinkingMovementRequestXML)))
+        }
+
+        resetMockServer()
+        startBackendService()
+
+        Thread.sleep(unavailablePeriodDurationInMillis)
+
+        1 to 5 foreach { _ =>
+          resetMockServer()
+          startBackendService()
+          await(sendValidXml(ValidInventoryLinkingMovementRequestXML))
+          verifyImportsConnectorServiceWasCalledWith(GoodsArrivalConnectorContext, ValidInventoryLinkingMovementRequestXML.toString())
+        }
+      }
+
+      "return a failed future when service returns 404" in {
+        startBackendService(NOT_FOUND)
+
+        intercept[RuntimeException](await(sendValidXml(ValidInventoryLinkingMovementRequestXML))).getCause.getClass shouldBe classOf[NotFoundException]
+      }
+
+      "return a failed future when service returns 400" in {
+        startBackendService(BAD_REQUEST)
+
+        intercept[RuntimeException](await(sendValidXml(ValidInventoryLinkingMovementRequestXML))).getCause.getClass shouldBe classOf[BadRequestException]
+      }
+
+      "return a failed future when service returns 500" in {
+        startBackendService(INTERNAL_SERVER_ERROR)
+
+        intercept[Upstream5xxResponse](await(sendValidXml(ValidInventoryLinkingMovementRequestXML)))
+      }
+
+      "return a failed future when connection with backend service fails" in {
+        stopMockServer()
+
+        intercept[RuntimeException](await(sendValidXml(ValidInventoryLinkingMovementRequestXML))).getCause.getClass shouldBe classOf[BadGatewayException]
+
+        startMockServer()
+      }
+  }
+
+  private def startBackendService(status: Int = ACCEPTED) =
+    setupBackendServiceToReturn(GoodsArrivalConnectorContext, status)
+
+  private def sendValidXml(xml:NodeSeq)(implicit vpr: ValidatedPayloadRequest[_]) =
+    connector.send(new GoodsArrival(), xml, new DateTime(), correlationId)
+}
