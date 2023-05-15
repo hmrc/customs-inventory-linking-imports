e8167b1bd80a800a1c343ff3ead41e1096321ffe
Drew Miley <drew.miley@testdrivensolutions.co.uk>
2023-05-15 12:06:28 +0100

    DCWL-807 Updates wiremock dependency to fix most IT

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,7 +6,7 @@ object AppDependencies {
 
   val scalaTestPlusPlay     = "org.scalatestplus.play"                    %% "scalatestplus-play"       % "5.1.0"          % testScope
   val scalatestplusMockito  = "org.scalatestplus"                         %% "scalatestplus-mockito"    % "1.0.0-M2"   % testScope
-  val wireMock              = "com.github.tomakehurst"                     % "wiremock-jre8"            % "2.34.0" % testScope
+  val wireMock              = "com.github.tomakehurst"                     % "wiremock-standalone"      % "2.27.2" % testScope
   val mockito               =  "org.mockito"                               % "mockito-core"             % "4.8.0" % testScope
   val flexmark              = "com.vladsch.flexmark"                       % "flexmark-all"             % "0.19.1" % testScope
   val Jackson               = "com.fasterxml.jackson.module"              %%  "jackson-module-scala"    % "2.14.0-rc1" % testScope
