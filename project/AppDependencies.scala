bdd7fdb13e6c9f2d6297e196a870963f1bed6109
Drew Miley <drew.miley@testdrivensolutions.co.uk>
2023-05-15 14:49:56 +0100

    DCWL-807 Updates dependencies as far as possible

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,9 +7,9 @@ object AppDependencies {
   val scalaTestPlusPlay     = "org.scalatestplus.play"                    %% "scalatestplus-play"       % "5.1.0"          % testScope
   val scalatestplusMockito  = "org.scalatestplus"                         %% "scalatestplus-mockito"    % "1.0.0-M2"   % testScope
   val wireMock              = "com.github.tomakehurst"                     % "wiremock-standalone"      % "2.27.2" % testScope
-  val mockito               =  "org.mockito"                               % "mockito-core"             % "4.8.0" % testScope
-  val flexmark              = "com.vladsch.flexmark"                       % "flexmark-all"             % "0.19.1" % testScope
-  val Jackson               = "com.fasterxml.jackson.module"              %%  "jackson-module-scala"    % "2.14.0-rc1" % testScope
-  val customsApiCommon      = "uk.gov.hmrc"                               %% "customs-api-common"       % "1.58.0"
-  val customsApiCommonTests = "uk.gov.hmrc"                               %% "customs-api-common"       % "1.58.0" % testScope classifier "tests"
+  val mockito               =  "org.mockito"                               % "mockito-core"             % "5.3.1" % testScope
+  val flexmark              = "com.vladsch.flexmark"                       % "flexmark-all"             % "0.35.10" % testScope
+  val Jackson               = "com.fasterxml.jackson.module"              %% "jackson-module-scala"     % "2.15.0" % testScope
+  val customsApiCommon      = "uk.gov.hmrc"                               %% "customs-api-common"       % "1.59.0"
+  val customsApiCommonTests = "uk.gov.hmrc"                               %% "customs-api-common"       % "1.59.0" % testScope classifier "tests"
 }
--- run_all_tests.sh
+++ run_all_tests.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-sbt clean scalastyle coverage test it:test coverageReport
+#sbt clean scalastyle coverage test it:test coverageReport dependencyUpdates
+sbt clean scalastyle coverage test it:test dependencyUpdates
