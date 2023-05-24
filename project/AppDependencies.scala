5db773fff04216e220e5ba7b626fb45f49f5666b
Drew Miley <drew.miley@testdrivensolutions.co.uk>
2023-05-24 15:34:02 +0100

    DCWL-807 Updates jackson module

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -10,7 +10,7 @@ object AppDependencies {
   val wireMock              = "com.github.tomakehurst"                     % "wiremock-standalone"      % "2.27.2" % testScope
   val mockito               =  "org.mockito"                               % "mockito-core"             % "5.3.1" % testScope
   val flexmark              = "com.vladsch.flexmark"                       % "flexmark-all"             % "0.35.10" % testScope
-  val Jackson               = "com.fasterxml.jackson.module"              %% "jackson-module-scala"     % "2.15.0" % testScope
+  val Jackson               = "com.fasterxml.jackson.module"              %% "jackson-module-scala"     % "2.15.1" % testScope
   val customsApiCommon      = "uk.gov.hmrc"                               %% "customs-api-common"       % apiCommonVersion
   val customsApiCommonTests = "uk.gov.hmrc"                               %% "customs-api-common"       % apiCommonVersion % testScope classifier "tests"
 }
