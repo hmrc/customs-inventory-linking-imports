466d3cb5e3bb9eae82c56246b84d5fd8b63c08b6
andrew-bintcliffe <186038219+andrew-bintcliffe@users.noreply.github.com>
2026-06-10 10:44:04 +0100

    DCWL-4473 | Remediate vulnerabilities

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object AppDependencies {
 
   val playVersion = "play-30"
-  val bootstrapVersion = "10.4.0"
+  val bootstrapVersion = "10.7.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                              %% s"bootstrap-backend-$playVersion" % bootstrapVersion,
@@ -11,7 +11,6 @@ object AppDependencies {
   )
 
   val test: Seq[ModuleID] = Seq(
-
     "org.scalatestplus"                        %% "mockito-4-11"                      % "3.2.18.0"       % Test,
     "org.wiremock"                              % "wiremock-standalone"               % "3.13.1"          % Test,
     "org.mockito"                               % "mockito-core"                      % "5.19.0"          % Test,
